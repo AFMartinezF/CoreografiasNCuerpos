79833c6e79f4e1526bfac72b69d587080b610f97
Angel Martinez <anmartinezf@unal.edu.co>
2022-06-16 10:26:03 -0500

    Primera versión funcional del código

--- 3Cuerpos.cpp
+++ 3Cuerpos.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 double distancia (double r_cuerpo1[][2], double r_cuerpo2[][2],int dim);
-double dv_euler(double  G, double m1, double m2, double r1, double r2);
+double diferencia(double r_cuerpo1[][2], double r_cuerpo2[][2]);
+double dv_euler_x(double  G, double m, double diferencia, double distancia);
 double dy_euler(double v);
 
 int main() {
@@ -31,30 +32,38 @@ int main() {
   cout << d12 << endl << d13 << endl << d23 << endl;
 
   //Ejecución del metodo de Euler (revisar)
-  v1[1][0] = v1[0][0] + dt*dv_euler(G,m[1],m[2],d12,d13);
-  cout<< v1[1][0] << endl;
+  cout<< v1[0][0] << endl;
+  v1[1][0] = v1[0][0] + dt*(dv_euler_x(G,m[1],diferencia(r1,r2),distancia(r1, r2,dim))+dv_euler_x(G,m[2],diferencia(r1,r3),distancia(r1, r3,dim)));
 
+  cout<< v1[1][0] << endl;
 
   return 0;
 }
 
 //Función que calcula la distancia entre dos puntos
 double distancia (double r_cuerpo1[][2], double r_cuerpo2[][2],int dim){
-  double dif [dim];
-  double d;
+  double diferencias [dim];
+  double distancia;
   for (int i = 0; i< dim; i++){
-    dif [i] = r_cuerpo1[i][i]-r_cuerpo2[i][i];
+    diferencias [i] = r_cuerpo1[i][i]-r_cuerpo2[i][i];
     //cout<<dif[i]<<endl;
   }
-  d = sqrt(pow(dif[0],2)+pow(dif[1],2));
-  return d;
+  distancia = sqrt(pow(diferencias[0],2)+pow(diferencias[1],2));
+  return distancia;
 }
 
-//Funciones del metodo de euler para una ecuación de segundo orden
-double dv_euler(double  G, double m1, double m2, double r1, double r2){
-  double v = -G*m1/pow(r1,2)-G*m2/pow(r2,2);
+double diferencia(double r_cuerpo1[][2], double r_cuerpo2[][2]){
+  double diferencia;
+  int i = 0;
+  diferencia = r_cuerpo1[i][i]-r_cuerpo2[i][i];
+  return diferencia;
+}
 
-  return v;
+
+//Funciones del metodo de euler para una ecuación de segundo orden
+double dv_euler_x(double  G, double m, double diferencia, double distancia){
+  double v = (-G*m/pow(distancia,2))*cos(diferencia/distancia); //Unir con las otras funciones? Llamar a las otras funciones?
+  return v;//generalizar
 }
 
 double dy_euler(double v){
