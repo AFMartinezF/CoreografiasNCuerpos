de1e13d8d85d83aa0338e51d32ac4056b9ade166
xmanuki <101660941+xmanuki@users.noreply.github.com>
2022-06-21 18:23:55 -0500

    Create Grafica.py
    graficas 2d

---
+++ Grafica.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+data = np.genfromtxt('Resultados.csv', delimiter=',')
+r1_x  = data[1:,0]
+r1_y  = data[1:,1]
+r2_x  = data[1:,2]
+r2_y  = data[1:,3]
+r3_x  = data[1:,4]
+r3_y  = data[1:,5]
+
+fig = plt.figure()
+ax = fig.gca()
+
+def actualizar(i):
+    ax.clear()
+    ax.plot(r1_x [:i], r1_y[:i])
+    ax.plot(r2_x [:i], r2_y[:i])
+    ax.plot(r3_x [:i], r3_y[:i])
+    plt.xlim(min(r3_x),max(r3_x))
+    plt.ylim(min(r3_y),max(r3_y))
+
+anim= animation.FuncAnimation(fig,actualizar,range(len(r1_x)))
+anim.save('df.gif')
