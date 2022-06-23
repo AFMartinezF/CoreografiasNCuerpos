1270b6bbc403cb04fe8c8a7895f90fd83c36c021
xmanuki <101660941+xmanuki@users.noreply.github.com>
2022-06-23 09:23:27 -0500

    Update SolGeneral.py
    ejemplo coreografia

--- SolGeneral.py
+++ SolGeneral.py
@@ -91,12 +91,12 @@ def LF(NPasos, tFinal, R, V): # posicion de un cuerpo respecto  matriz 3d
     RNew = np.zeros((NPasos+1, n, 3)) # RNew (tienpo ,numero de masas, cordenadas xyz) nueva posicion de cada cuerpo
     RNew[0] = R
     VNew = V
-    VOld = V - dt/2 * DV(n, M, R) # este es V n-1/2
+    VOld = V + dt/2 * DV(n, M, R) # este es V n-1/2
 
     for i in range(NPasos):
         
         VNew =  VOld +  dt * DV(n, M, RNew[i]) 
-        RNew[i+1] = RNew[i] + 2* dt * VNew
+        RNew[i+1] = RNew[i] +  dt * VOld
         VOld = VNew
 
     #RNew[:, 0, : ]) #posiciones xyz cuerpo 1
@@ -120,11 +120,12 @@ def Verlet(NPasos, tFinal, R, V): # posicion de un cuerpo respecto  matriz 3d
     dt = tFinal / NPasos
     RNew = np.zeros((NPasos+1, n, 3)) # RNew (tienpo ,numero de masas, cordenadas xyz) nueva posicion de cada cuerpo
     RNew[0] = R
-    ROld = R + V * -dt  # este es R n-1
+    ROld = R - V * dt  # este es R n-1
 
     for i in range(NPasos):
         
         RNew[i+1] = 2 * RNew[i] - ROld + dt**2 * DV(n, M, RNew[i]) 
+        ROld = RNew[i]
     
     #RNew[:, 0, : ]) #posiciones xyz cuerpo 1
     #RNew[:, 1, : ]) #posiciones xyz  cuerpo 2
@@ -146,18 +147,21 @@ def Verlet(NPasos, tFinal, R, V): # posicion de un cuerpo respecto  matriz 3d
 # condiciones iniciales 
 
 n = 3 # numero de masas (numero entero)
-M = [1,2,3] # masa de cada cuerpo
+M = [1,1,1] # masa de cada cuerpo
 
 R = np.zeros((n, 3)) #posicion de cada cuerpo (xyz en la horizontal)
-R[0, 2] = 1
-R[1, 1] = 1
-R[2, 0] =1
+R[0, 0] = -0.995492
+R[1, 0] = 0.995492
 
 V = np.zeros((n, 3)) #velocidad de cada cuerpo (xyz en la horizontal)
-V[0, 0] = 1
-V[1, 1] = 1
-V[2, 2] = 1
+#V[0, 0] = 1
+V[0, 0] = -0.347902
+V[1, 0] = -0.347902
+V[2, 0] =  0.695804 
+V[0, 1] = -0.533930
+V[1, 1] = -0.533930
+V[2, 1] =  1.067860
 
 
-RFinal = RK4(100,3,R,V)
+RFinal = RK4(1000,6,R,V)
 np.savetxt("SolGeneral.cvs ",RFinal ,delimiter=",")
